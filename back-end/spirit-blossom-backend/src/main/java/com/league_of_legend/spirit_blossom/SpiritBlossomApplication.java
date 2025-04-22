73f8c690e91b4bf5d83bf0b97f6e45dcd19fb310
xuanphuda <xuanphuda@gmail.com>
2025-04-22 15:46:12 +0700

    fix: redefine the structure of project folders >> the project folders contain two big seperate folders: front-end and back-end

---
+++ back-end/spirit-blossom-backend/src/main/java/com/league_of_legend/spirit_blossom/SpiritBlossomApplication.java
@@ -0,0 +1,13 @@
+package com.league_of_legend.spirit_blossom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiritBlossomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiritBlossomApplication.class, args);
+	}
+
+}
---
+++ back-end/spirit-blossom-backend/src/test/java/com/league_of_legend/spirit_blossom/SpiritBlossomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.league_of_legend.spirit_blossom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiritBlossomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
